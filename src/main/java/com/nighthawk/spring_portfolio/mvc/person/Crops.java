a8bda24d00ac11d567af66c5a344d836954033e8
Tirth-Thakkar <tirth1226@gmail.com>
2024-02-23 22:08:03 -0800

    adding some quick changes to start on FRQ code

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Crops.java
@@ -0,0 +1,11 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+public interface Crops {
+    int econValue();
+
+    int growthStage();
+
+    int profitValue();
+
+    int waterUsage();
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/SoilAdditives.java
@@ -0,0 +1,11 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+public interface SoilAdditives {
+    int cost();
+
+    int ecoEffect();
+
+    int growthEffect();
+
+    int profitEffect();
+}
