9b556a04fc08efc4fb8a60ca5f34ad26e905cfad
h4seeb-cmd <mirzahbeg123@gmail.com>
2024-02-23 22:44:00 -0800

    adding growthStage functionality

--- src/main/java/com/nighthawk/spring_portfolio/mvc/person/Corn.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Corn.java
@@ -1,14 +1,23 @@
 package com.nighthawk.spring_portfolio.mvc.person;
+import java.lang.Math;
 
 public class Corn implements Crops {
     @Override
     public int econValue() {
-        return 0;
+        return 1;
     }
 
     @Override
     public int growthStage() {
-        return 0;
+        // get week via request from FE
+        double randNum = 2 + (math.random() * 4); // generates a random number between 2 and 5
+        int weekGrowthFactor = int(randNum);
+        int developmentStage;
+        int initialWeek; // week when corn was placed
+        int week; 
+        if(week - initialWeek == weekGrowthFactor){
+            developmentStage = developmentStage + 1;
+        }
     }
 
     @Override
