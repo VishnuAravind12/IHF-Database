5ba72b574b2e680fa1202b626c327bf5b39bbcd6
Tirth-Thakkar <tirth1226@gmail.com>
2024-02-23 22:11:33 -0800

    Adding classes that implement the interfaces

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Corn.java
@@ -0,0 +1,23 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+public class Corn implements Crops {
+    @Override
+    public int econValue() {
+        return 0;
+    }
+
+    @Override
+    public int growthStage() {
+        return 0;
+    }
+
+    @Override
+    public int profitValue() {
+        return 0;
+    }
+
+    @Override
+    public int waterUsage() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/person/Glyphosate.java
@@ -0,0 +1,23 @@
+package com.nighthawk.spring_portfolio.mvc.person;
+
+public class Glyphosate implements SoilAdditives {
+    @Override
+    public int cost() {
+        return 0;
+    }
+
+    @Override
+    public int ecoEffect() {
+        return 0;
+    }
+
+    @Override
+    public int growthEffect() {
+        return 0;
+    }
+
+    @Override
+    public int profitEffect() {
+        return 0;
+    }
+}
